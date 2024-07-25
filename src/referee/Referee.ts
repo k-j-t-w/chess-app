b541f79971ffbdc6661291c02a3182b69652280f
Kai Wingfield <kaiwingfield17@gmail.com>
2024-07-25 15:19:29 -0700

    finish implementing non-attacking pawn movement

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,7 +1,26 @@
-import { PieceType, TeamType } from "../components/Chessboard";
+import { PieceType, TeamType, Piece } from "../components/Chessboard";
 
 export default class Referee {
-  isValidMove(px: number, py: number, x: number, y: number, type: PieceType, team: TeamType) {
+  tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
+    console.log("Checking if tile is occupied...")
+
+    const piece = boardState.find(p => p.x === x && p.y === y)
+    if (piece) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isValidMove(
+    px: number, 
+    py: number, 
+    x: number, 
+    y: number, 
+    type: PieceType, 
+    team: TeamType,
+    boardState: Piece[]
+  ) {
     console.log('Referee is checking the move...')
     console.log('Previous Location: ', px, py)
     console.log('Current Location: ', x, y)
@@ -9,21 +28,19 @@ export default class Referee {
     console.log('TeamType: ', team)
 
     if (type === PieceType.PAWN) {
-      if (team === TeamType.WHITE) {
-        if (py === 6) {
-          if (px === x && (py - y === 1 || py - y === 2)) {
-            console.log('Valid Move! :)');
-            return true;
-          }
-        } else {
-          if (px === x && py - y === 1) {
-            console.log('Valid Move! :)');
-            return true;
-          }
+      const specialRow = (team === TeamType.WHITE) ? 6 : 1;
+      const pawnDirection =(team === TeamType.WHITE) ? 1 : -1
+
+      if(px === x &&py === specialRow && py - y === 2*pawnDirection) {
+        if (!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y+pawnDirection, boardState)) {
+          return true;
+        }
+      } else if (px === x && py - y === pawnDirection) {
+        if (!this.tileIsOccupied(x, y, boardState)){
+          return true;
         }
       }
     }
-
     return false;
   }
 }
