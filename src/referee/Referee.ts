182d869ff29b1acbae03d99aa3c7f957329c0ebc
Kai Wingfield <kaiwingfield17@gmail.com>
2024-07-28 18:55:52 -0700

    finish all piece movement logic

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -83,6 +83,229 @@ export default class Referee {
           return true;
         }
       } 
+      // BISHOP LOGIC
+    } else if (type === PieceType.BISHOP) {
+
+      if (Math.abs(desiredPosition.x - initialPosition.x) === Math.abs(desiredPosition.y - initialPosition.y))  {
+        // bottom right logic
+        if (desiredPosition.x > initialPosition.x && desiredPosition.y > initialPosition.y ) {
+          for(let i = 0; i < desiredPosition.x - initialPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x + 1 + i, initialPosition.y + 1 + i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // top right logic
+        } else if (desiredPosition.x > initialPosition.x && desiredPosition.y < initialPosition.y) {
+          for(let i = 0; i < desiredPosition.x - initialPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x + 1 + i, initialPosition.y - 1 - i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // top left
+        } else if (desiredPosition.x < initialPosition.x && desiredPosition.y < initialPosition.y) {
+          for(let i = 0; i < initialPosition.x - desiredPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x - 1 - i, initialPosition.y - 1 - i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          //bottom left
+        } else if (desiredPosition.x < initialPosition.x && desiredPosition.y > initialPosition.y) {
+          for(let i = 0; i < initialPosition.x - desiredPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x - 1 - i, initialPosition.y + 1 + i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+        }
+      }
+    // ROOK LOGIC
+    } else if (type === PieceType.ROOK) {
+      if ((desiredPosition.x - initialPosition.x !== 0 && desiredPosition.y === initialPosition.y ) || (desiredPosition.y - initialPosition.y !== 0 && desiredPosition.x === initialPosition.x))  {
+        // left logic
+        if (desiredPosition.x < initialPosition.x && desiredPosition.y === initialPosition.y) {
+          for(let i = 0; i < initialPosition.x - desiredPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x - 1 - i, initialPosition.y, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // right logic
+        } else if (desiredPosition.x > initialPosition.x && desiredPosition.y === initialPosition.y) {
+          for(let i = 0; i < desiredPosition.x - initialPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x + 1 + i, initialPosition.y, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // up logic
+        } else if (desiredPosition.x === initialPosition.x && desiredPosition.y < initialPosition.y) {
+          for(let i = 0; i < initialPosition.y - desiredPosition.y - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x, initialPosition.y - 1 - i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // down logic
+        } else if (desiredPosition.x === initialPosition.x && desiredPosition.y > initialPosition.y) {
+          for(let i = 0; i < desiredPosition.y - initialPosition.y - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x, initialPosition.y + 1 + i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+        }
+      }
+      // QUEEN LOGIC
+    } else if (type === PieceType.QUEEN) {
+      if ((desiredPosition.x - initialPosition.x !== 0 && desiredPosition.y === initialPosition.y ) || (desiredPosition.y - initialPosition.y !== 0 && desiredPosition.x === initialPosition.x))  {
+        // left logic
+        if (desiredPosition.x < initialPosition.x && desiredPosition.y === initialPosition.y) {
+          for(let i = 0; i < initialPosition.x - desiredPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x - 1 - i, initialPosition.y, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // right logic
+        } else if (desiredPosition.x > initialPosition.x && desiredPosition.y === initialPosition.y) {
+          for(let i = 0; i < desiredPosition.x - initialPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x + 1 + i, initialPosition.y, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // up logic
+        } else if (desiredPosition.x === initialPosition.x && desiredPosition.y < initialPosition.y) {
+          for(let i = 0; i < initialPosition.y - desiredPosition.y - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x, initialPosition.y - 1 - i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // down logic
+        } else if (desiredPosition.x === initialPosition.x && desiredPosition.y > initialPosition.y) {
+          for(let i = 0; i < desiredPosition.y - initialPosition.y - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x, initialPosition.y + 1 + i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+        }
+      } else       if (Math.abs(desiredPosition.x - initialPosition.x) === Math.abs(desiredPosition.y - initialPosition.y))  {
+        // bottom right logic
+        if (desiredPosition.x > initialPosition.x && desiredPosition.y > initialPosition.y ) {
+          for(let i = 0; i < desiredPosition.x - initialPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x + 1 + i, initialPosition.y + 1 + i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // top right logic
+        } else if (desiredPosition.x > initialPosition.x && desiredPosition.y < initialPosition.y) {
+          for(let i = 0; i < desiredPosition.x - initialPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x + 1 + i, initialPosition.y - 1 - i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          // top left
+        } else if (desiredPosition.x < initialPosition.x && desiredPosition.y < initialPosition.y) {
+          for(let i = 0; i < initialPosition.x - desiredPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x - 1 - i, initialPosition.y - 1 - i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+          //bottom left
+        } else if (desiredPosition.x < initialPosition.x && desiredPosition.y > initialPosition.y) {
+          for(let i = 0; i < initialPosition.x - desiredPosition.x - 1; i++) {
+            if (this.tileIsOccupied(initialPosition.x - 1 - i, initialPosition.y + 1 + i, boardState)) {
+              return false;
+            }
+          }
+          if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+            return true;
+          } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+            return true;
+          }
+        }
+      }
+    // KING LOGIC
+    } else if (type === PieceType.KING) {
+      if (
+        (Math.abs(desiredPosition.x - initialPosition.x) === 1 && Math.abs(desiredPosition.y - initialPosition.y) === 1) ||
+        (Math.abs(desiredPosition.x - initialPosition.x) === 1 && desiredPosition.y === initialPosition.y) ||
+        (Math.abs(desiredPosition.y - initialPosition.y) === 1 && desiredPosition.x === initialPosition.x)
+      ) {
+        if(!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState)) {
+          return true;
+        } else if (this.tileIsOccupiedByOppenent(desiredPosition.x, desiredPosition.y, boardState, team)) {
+          return true;
+        }
+      }
     }
 
     return false;
